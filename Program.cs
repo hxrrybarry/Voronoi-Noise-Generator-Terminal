20936366e98de08fe6f2af6ce459b77ba3cf29ad
hxrrybarry <145754861+hxrrybarry@users.noreply.github.com>
2024-02-26 23:25:23 +0000

    Added some comments. Moved Arr2Str method to VoronoiNoise.GetString. Added / changed some controls.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Emit;
 
 namespace Voronoi;
 
@@ -7,16 +8,18 @@ internal class Program
     const char FILLED = '#';
     const char UNFILLED = ' ';
 
-    private class VoronoiNoise(int xSize, int ySize, int zSize, int numberOfPoints, float threshold)
+    private class VoronoiNoise(int xSize, int ySize, int zSize, int numberOfPoints, float threshold, int seed)
     {
-        public int XSize { get; } = xSize;
-        public int YSize { get; } = ySize;
+        private int XSize { get; } = xSize;
+        private int YSize { get; } = ySize;
         public int ZSize { get; } = zSize;
         private int NumberOfPoints { get; } = numberOfPoints;
         private float Threshold { get; } = threshold;
 
         public char[,,] FilledGrid { get; set; } = new char[xSize, ySize, zSize];
         private int[,] PointsOnGrid { get; } = new int[numberOfPoints, 3];
+
+        public int Seed { get; } = seed;
 
         private float GetDistanceToNearestPoint(int x, int y, int z)
         {
@@ -25,6 +28,7 @@ internal class Program
             float nearestDistance = 1000;
             for (int i = 0; i < NumberOfPoints; i++)
             {
+                // distance formula: s = √((x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2)
                 float distance = MathF.Sqrt(MathF.Pow(x - PointsOnGrid[i, 0], 2) + MathF.Pow(y - PointsOnGrid[i, 1], 2) + MathF.Pow(z - PointsOnGrid[i, 2], 2));
                 if (distance < nearestDistance)
                     nearestDistance = distance;
@@ -33,9 +37,9 @@ internal class Program
             return nearestDistance;
         }
 
-        public void GenerateGrid(int seed)
+        public void GenerateGrid()
         {
-            Random r = new(seed);
+            Random r = new(Seed);
 
             // choose NumberOfPoints amount of random coordinates to-
             //- sit on the grid for later point-distance calculations
@@ -46,6 +50,7 @@ internal class Program
                 PointsOnGrid[i, 2] = r.Next(ZSize);
             }
 
+            // *this is yikes but no other real solution
             // loop through each point (very slow in 3D!) and perform-
             // - a distance calculation which is then evaluated at a threshold-
             // - to determine whether to fill that point with a value
@@ -57,7 +62,7 @@ internal class Program
                     {
                         float distance = GetDistanceToNearestPoint(x, y, z);
 
-                        if (distance > Threshold)
+                        if (distance < Threshold)
                             FilledGrid[x, y, z] = FILLED;
                         else
                             FilledGrid[x, y, z] = UNFILLED;
@@ -65,49 +70,58 @@ internal class Program
                 }
             }
         }
-    }
 
-    private static string Arr2Str(VoronoiNoise n, int level)
-    {
-        string result = "";
-
-        // append to string a particular slice of the 3D noise
-        // z coordinate is determined by "level"
-        for (int i = 0; i < n.XSize; i++)
+        public string ToString(int level)
         {
-            result += '\n';
-            for (int j = 0; j < n.YSize; j++)
-                result += n.FilledGrid[i, j, level];
-        }
+            string result = "";
 
-        return result;
+            // append to string a particular slice of the 3D noise
+            // z coordinate is determined by "level"
+            for (int i = 0; i < XSize; i++)
+            {
+                result += '\n';
+                for (int j = 0; j < YSize; j++)
+                    result += FilledGrid[i, j, level];
+            }
+
+            return result;
+        }
     }
+
 
     public static void Main()
     {
-        VoronoiNoise noise = new(27, 125, 54, 750, 3.5f);
-
-        int seed = new Random().Next();
-        noise.GenerateGrid(seed);
+        // worth noting X and Y coordinate are flipped - cba fixing doesn't really matter
+        VoronoiNoise noise = new(27, 54, 1, 50, 3.5f, 189453129);
+        noise.GenerateGrid();
+        
         int level = 0;
-    
         while (true)
         {
-            Console.Clear();
-            Console.WriteLine($"{Arr2Str(noise, level)}\nSeed: {seed}, Z Level: {level}");
+            Console.WriteLine($"{noise.ToString(level)}\nSeed: {noise.Seed}, Z Level: {level}");
            
             ConsoleKeyInfo keyPress = Console.ReadKey();
             
-            if (keyPress.Key == ConsoleKey.RightArrow)
+            if (keyPress.Key == ConsoleKey.RightArrow || keyPress.Key == ConsoleKey.D)
             {
                 if (level != noise.ZSize - 1)
                     level++;
+                else
+                    level = 0;
             }
-            else if (keyPress.Key == ConsoleKey.LeftArrow)
+            else if (keyPress.Key == ConsoleKey.LeftArrow || keyPress.Key == ConsoleKey.A)
             {
                 if (level != 0)
                     level--;
+                else
+                    level = noise.ZSize - 1;
             }
+            else if (keyPress.Key == ConsoleKey.Enter)
+            {
+                noise = new(28, 56, 56, 750, 3.5f, new Random().Next());
+                noise.GenerateGrid();
+            }
+            Console.Clear();
         }      
     }
 }
